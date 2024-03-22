1a45b1df44da0fc27085275bbd905a9906a72601
AYUSHDEVANI <ayushdevani0018@gmail.com>
2024-03-22 23:38:26 +0530

    Create function for get weather using api and return data in json formate.

--- Weather_App.py
+++ Weather_App.py
@@ -1,2 +1,7 @@
 
 import requests
+
+def get_weather(api_key, city):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    return response.json()    
