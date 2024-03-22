0bfb7c7e751d924e967dfc99abe8599b1dbbc86c
AYUSHDEVANI <ayushdevani0018@gmail.com>
2024-03-22 23:44:57 +0530

    Create function for get forecast and return data into json formate.

--- Weather_App.py
+++ Weather_App.py
@@ -14,3 +14,8 @@ def display_current_weather(data):
         print(f"Weather: {data["weather"][0]["description"]}")
     else:
         print("City not found. Please try again.")
+
+def get_forecast(api_key, city):
+    url = f"http://api.openweathermap.org/data/2.5/forecast?q={city}&appid={api_key}&units=metric"
+    response = requests.get(url)
+    return response.json()
