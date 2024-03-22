f71d2d1e9229e0376639c1cd7ba93d476f5480fe
AYUSHDEVANI <ayushdevani0018@gmail.com>
2024-03-22 23:43:11 +0530

    Create function for display weather.

--- Weather_App.py
+++ Weather_App.py
@@ -5,3 +5,12 @@ def get_weather(api_key, city):
     url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     response = requests.get(url)
     return response.json()    
+
+def display_current_weather(data):
+    if data["cod"] == 200:
+        print(f"Weather in {data["name"]}:")
+        print(f"Temperature: {data["main"]["temp"]}°C")
+        print(f"Humidity: {data["main"]["humidity"]}%")
+        print(f"Weather: {data["weather"][0]["description"]}")
+    else:
+        print("City not found. Please try again.")
